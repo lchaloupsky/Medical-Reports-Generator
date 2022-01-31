170a59621fe746f46cfe9035f2e1227add65ef4c
Lukas Chaloupsky <lukaschaloupsky@seznam.cz>
2022-01-31 13:36:09 +0100

    Fixed line_starts preprocessor.

--- preprocessors/line_starts_preprocessor.py
+++ preprocessors/line_starts_preprocessor.py
@@ -7,7 +7,7 @@ class LineStartsPreprocessor(Preprocessor):
         '''Takes away all whitespaces at the start/end of a line which are common for all lines.'''
 
         # split text
-        line_splits = text.split('\n')
+        line_splits = text.rstrip('\n').split('\n')
 
         # find minimum number of white spaces of all lines on both sides
         min_whitespaces_left = min(map(lambda x: len(x) - len(x.lstrip()), line_splits))
